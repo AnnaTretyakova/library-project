2230baa3d6b339c68ab8e7e53bcb7db71a03dc6a
AnnaTretyakova <anntretwork@gmail.com>
2023-06-07 03:51:14 +0200

    add users class and repository, try to create server to read user data from db, but have an error now

---
+++ src/main/java/ru/itgirl/libraryproject/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package ru.itgirl.libraryproject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.itgirl.libraryproject.model.Users;
+
+public interface UserRepository extends JpaRepository<Users, Long> {
+
+    <Optional>Users findUsersByUsername(String userName);
+}
